9fd06a7c679a5dce4b7a3536c1f47ce4502425ac
Jaskaran-1914294 <jaskaran@Jaskarans-MacBook-Air.local>
2022-02-02 10:32:29 +0530

    Update for image

--- app/src/main/java/com/example/sampleapk/MainActivity.java
+++ app/src/main/java/com/example/sampleapk/MainActivity.java
@@ -7,9 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    int i=0;
+
     public void buttonPressed(View view){
         EditText nameEntered=(EditText)findViewById(R.id.nameEditText);
         String name =nameEntered.getEditableText().toString();
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
         Log.i("password - ",passwordEntered.toString());
         nameEntered.setText("");
         passwordEntered.setText((""));
+        ImageView image=(ImageView)findViewById(R.id.imageView);
+        if(i%2==0){
+            image.setImageResource(R.drawable.cat2);
+        }
+        else{
+            image.setImageResource((R.drawable.cat1));
+        }
+        i++;
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
